7fd98020a6af8523f1f3735fa0da3a1c4494f999
Andy R <xnorlogic@gmail.com>
2019-03-14 09:31:44 -0400

    Update MonotonicCheck.c
    Updated data types and code cleanup

--- Checks/MonotonicCheck.c
+++ Checks/MonotonicCheck.c
@@ -1,25 +1,23 @@
-U8 Monotonically_Inc_Chk(BarCode *GenericBarCode,U8 DATA_OFFSET){
-	U8 Index = 0U;
-	U8 Monotonically_Inc_Cnt = 0U;
-	U8 Monotonically_Inc_Chk_Flag = 0U;
-	F32 buffer_1 = 0.0f;
-	F32 buffer_2 = 0.0f;
+unsigned char Monotonically_Inc_Chk(BarCode *GenericBarCode,unsigned char DATA_OFFSET){
+	unsigned char Index = 0U;
+	unsigned char MonotonicallyIncCnt = 0U;
+	unsigned char DataCheckSize = DATA_SIZE - 1U;
+	float buffer_1 = 0.0f;
+	float buffer_2 = 0.0f;
 
-	for (Index = 0U; Index < DATA_SIZE - 1U; Index++) {
+	for (Index = 0U; Index < DataCheckSize; Index++) {
 		/* Extract and scale data element from the barcode data*/
 		buffer_1 = GenericBarCode->Data[Index + DATA_OFFSET];
-		buffer_1 /= DATA_SCALE_FACTOR;
+		buffer_1 *= DATA_SCALE_FACTOR;
 		/* Extract and scale next data element from the barcode data*/
 		buffer_2 = GenericBarCode->Data[(Index + DATA_OFFSET) + 1U];
-		buffer_2 /= DATA_SCALE_FACTOR;
-		/* Compare for monotonically increasing data */
+		buffer_2 *= DATA_SCALE_FACTOR;
+		/* Compare for monotonic increase data pair */
 		if (buffer_1 <= buffer_2) {
-			Monotonically_Inc_Cnt = Monotonically_Inc_Cnt + 1U;
+			MonotonicallyIncCnt = MonotonicallyIncCnt + 1U;
 		} 
 	}
 
-	/* Torque Monotonically increasing if and only if all data is increasing */
-	Monotonically_Inc_Chk_Flag = (Monotonically_Inc_Cnt == TORQUE_CURVE_DATA_SIZE - 1U);
-
-	return Monotonically_Inc_Chk_Flag;
+	/* Data Monotonically increasing if and only if all data is increasing */
+	return (MonotonicallyIncCnt == DataCheckSize);
 }
