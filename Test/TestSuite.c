295dd4c515332ccc28a8c1f141aecbaf3c022670
Andy R <xnorlogic@gmail.com>
2019-03-19 08:35:58 -0400

    Update TestSuite.c
    Update to the sort test set

--- Test/TestSuite.c
+++ Test/TestSuite.c
@@ -7,7 +7,7 @@
 
 #define MONOTONIC_CHK_PASS_ID  3U
 #define CHECKSUM_CHK_PASS_ID 4U
-#define SORT_CHK_PASS_ID 5U
+#define SORT_CHK_PASS_ID 6U
 
 BarCode GenericBarCode = {
 	0U, /*SID*/
@@ -125,6 +125,24 @@ uint_8 SortTest_A(void){
 	uint_8 data3[6]={2,7,6,7,9,0};
 	uint_8 data4[6]={5,6,6,6,6,1};
 
+	uint_8 data5[255]={200,192,185,25,88,137,220,253,250,198,13,222,130,55,
+			             1,75,124,135,17,221,93,36,176,95,191,172,170,254,
+						 17,176,121,127,36,71,251,125,111,65,164,105,187,239,
+						 206,5,18,166,102,151,246,122,104,205,6,160,25,228,48,
+						 148,173,182,68,113,30,151,134,144,29,221,0,4,101,211,
+						 75,249,162,21,143,14,134,204,42,179,5,237,158,128,45,
+						 10,187,247,100,102,84,21,229,52,104,243,20,14,241,7,
+						 176,46,123,42,96,175,177,164,252,208,198,159,155,48,210,
+						 165,171,207,161,52,237,9,227,120,58,35,221,240,225,177,
+						 245,18,56,226,22,218,27,3,15,154,187,10,157,35,178,92,24,
+						 157,249,243,130,209,215,143,126,250,159,242,53,28,19,123,
+						 158,159,102,5,15,50,21,66,60,135,108,91,145,51,50,27,142,
+						 202,162,46,164,226,233,106,253,153,137,112,218,6,36,177,
+						 131,146,29,103,195,32,116,215,13,103,179,81,113,232,138,
+						 126,102,22,183,20,91,174,137,132,179,231,254,6,231,236,
+						 194,28,195,43,83,212,120,95,36,48,234,107,150,80,101,122,
+						 69,3,138,41,199,243,201,122,245,133,185,20,131};
+
 	SortCheck = Monotonically_Inc_Chk_GEN((uint_8*)&data0,sizeof(data0)) + SortCheck;
 	Bubble_Sort((uint_8*)&data0,sizeof(data0));
 	SortCheck = Monotonically_Inc_Chk_GEN((uint_8*)&data0,sizeof(data0)) + SortCheck;
@@ -144,6 +162,10 @@ uint_8 SortTest_A(void){
 	SortCheck = Monotonically_Inc_Chk_GEN((uint_8*)&data4,sizeof(data4)) + SortCheck;
 	Bubble_Sort((uint_8*)&data4,sizeof(data4));
 	SortCheck = Monotonically_Inc_Chk_GEN((uint_8*)&data4,sizeof(data4)) + SortCheck;
+
+	SortCheck = Monotonically_Inc_Chk_GEN((uint_8*)&data5,sizeof(data5)) + SortCheck;
+	Bubble_Sort((uint_8*)&data5,sizeof(data5));
+	SortCheck = Monotonically_Inc_Chk_GEN((uint_8*)&data5,sizeof(data5)) + SortCheck;
 
 	return (SortCheck == SORT_CHK_PASS_ID);
 }
