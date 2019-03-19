4e69bfefa31b2738138f3d5325d04a1367305f39
Andy R <xnorlogic@gmail.com>
2019-03-19 06:46:48 -0400

    Update Test.bat
    added comments and GCC_PATH

--- TestSuite/Test.bat
+++ TestSuite/Test.bat
@@ -1,16 +1,19 @@
 cls
 echo off
 echo Test Suite for MiscFunctions has started...
+rem GCC toolpath MinGW
+SET GCC_PATH=C:\MinGW\bin
+rem Project architecture
 SET APPLICATION_NAME=TestApp
 SET FUNCTIONS_PATH=..\Functions
 SET PLATFORM_PATH=..\Platform
 SET TEST_PATH=..\Test
 SET OBJECT_FILES=TestSuite.o MonotonicCheck.o CheckSumCalc.o
 rem Compile and build
-gcc -c %FUNCTIONS_PATH%\MonotonicCheck.c -I %FUNCTIONS_PATH% -I %PLATFORM_PATH% -o MonotonicCheck.o
-gcc -c %FUNCTIONS_PATH%\CheckSumCalc.c -I %FUNCTIONS_PATH% -I %PLATFORM_PATH% -o CheckSumCalc.o
-gcc -c %TEST_PATH%\TestSuite.c -I %FUNCTIONS_PATH% -I %PLATFORM_PATH% -o TestSuite.o
-gcc %OBJECT_FILES% -o %APPLICATION_NAME%.exe
+%GCC_PATH%\gcc -c %FUNCTIONS_PATH%\MonotonicCheck.c -I %FUNCTIONS_PATH% -I %PLATFORM_PATH% -o MonotonicCheck.o
+%GCC_PATH%\gcc -c %FUNCTIONS_PATH%\CheckSumCalc.c -I %FUNCTIONS_PATH% -I %PLATFORM_PATH% -o CheckSumCalc.o
+%GCC_PATH%\gcc -c %TEST_PATH%\TestSuite.c -I %FUNCTIONS_PATH% -I %PLATFORM_PATH% -o TestSuite.o
+%GCC_PATH%\gcc %OBJECT_FILES% -o %APPLICATION_NAME%.exe
 @echo Build Complete
 rem cleanup
 rm TestSuite.o
